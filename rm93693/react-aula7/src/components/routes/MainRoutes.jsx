7e1eb92b3260caaad2f829977f785f8154d1f448
phalves23 <rm93693@fiap.com.br>
2022-09-13 10:00:51 -0300

    :sparkles: criando rotas

---
+++ rm93693/react-aula7/src/components/routes/MainRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../home/Home";
+import ListaTarefas from "../listaTarefas/ListaTarefas";
+import Tarefa from "../tarefa/Tarefa";
+
+export default function MainRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lt" element={<ListaTarefas />} />
+        <Route path="/add" element={<Tarefa />} />
+      </Routes>
+    </>
+  );
+}
