eadedd0d327368b3cdb83f1dbd2c31ee27a2ca81
btwbeatriz <rm95246@fiap.com.br>
2022-09-13 08:46:57 -0300

    Tarefa.jsx criado

--- rm95246/react-aula7/src/components/ListaTarefas.jsx
+++ rm95246/react-aula7/src/components/ListaTarefas.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Tarefa from './Tarefa';
 
 export default function ListaTarefas(){
     return(
         
         <div>
             <p>Lista de Tarefas</p>
+            <div>
+                <Tarefa/>
+                <Tarefa/>
+                <Tarefa/>
+            </div>
         </div>
 
     )
---
+++ rm95246/react-aula7/src/components/Tarefa.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Tarefa(){
+    return(
+        
+        <div>
+            <h2>Relatório de vendas</h2>
+            <p>Para: Departamento de Vendas</p>
+            <p>Levantar os valores de vendas deste mês.</p>
+        </div>
+
+    )
+}
