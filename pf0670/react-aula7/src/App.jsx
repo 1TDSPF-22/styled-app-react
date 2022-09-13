17e75b7dc800d1261081aa0d0a65cf3d76b4578e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-13 08:42:44 -0300

    index.css + ListaTarefa.jsx criados.

--- pf0670/react-aula7/src/App.jsx
+++ pf0670/react-aula7/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import ListaTarefas from './components/ListaTarefas'
 
 export default function App() {
     return (
         <div>
 
             <h1>ESTUDOS DE STYLED-COMPONENTES</h1>
-
+            <ListaTarefas />
         </div>
     )
 }
