0ab786e6800cf384069e49fbf9cbcb037c3bf86b
augustopdro <rm94990@fiap.com.br>
2022-09-13 08:42:59 -0300

    index.css e components criados.

--- rm94990/react-aula7/src/App.jsx
+++ rm94990/react-aula7/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ListaTarefas from "./components/ListaTarefas";
 
 export default function App() {
   return (
     <div>
       <h1>ESTUDOS DE STYLED-COMPONENTS</h1>
+      <ListaTarefas />
     </div>
   );
 }
