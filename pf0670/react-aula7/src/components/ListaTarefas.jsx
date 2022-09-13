3403c93e0b6ec121467e6a9fa9e371b5e4e21f49
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-13 09:32:47 -0300

    addTarefas criado

--- pf0670/react-aula7/src/components/ListaTarefas.jsx
+++ pf0670/react-aula7/src/components/ListaTarefas.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tarefa from './Tarefa'
 import styled from 'styled-components'
 
@@ -13,14 +13,59 @@ const DivLista = styled.div`
 `
 
 export default function ListaTarefas() {
+
+    const [tarefa, setTarefa] = useState([
+        {
+            titulo: "Lista de Pagamento",
+            setor: "Dep. Vendas",
+            descricao: "Levantar os valores de Vendas."
+        }
+        ,
+        {
+            titulo: "Planilha de Salários",
+            setor: "Dep. Pessoal",
+            descricao: "Gerar Planilhas"
+        }
+        ,
+        {
+            titulo: "Lançar Notas do Challenger",
+            setor: "Graduação",
+            descricao: "Lançar notas dos grupos no portal Sprint 3."
+        }
+    ])
+
+    //CRIANDO UMA FUNÇÃO PARA ADICIONAR TAREFAS
+    const addTarefa = () => {
+
+        const novaTarefa = {
+            titulo: "Planilha de Notas",
+            setor: "Graduação",
+            descricao: "Lançar quem não fez os exercícios."
+        }
+
+        setTarefa([...tarefa, novaTarefa])
+    }
+
+
     return (
         <div>
             <p>Lista de Tarefas</p>
+            <div>
+                <button onClick={addTarefa}>ADD-TAREFA</button>
+            </div>
             <DivLista>
-                <Tarefa />
-                <Tarefa />
-                <Tarefa />
+                {tarefa.map((t, i) =>
+                    <Tarefa
+                        key={i}
+                        id={i + 1}
+                        titulo={t.titulo}
+                        setor={t.setor}
+                        descricao={t.descricao}
+                    />
+                )}
+
             </DivLista>
+
         </div>
     )
 }
