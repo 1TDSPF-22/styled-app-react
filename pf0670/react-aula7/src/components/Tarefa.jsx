b0a202bbe345f5dd68e0fb522d5cebd7dbe73480
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-13 08:46:43 -0300

    Tarefa.jsx add

---
+++ pf0670/react-aula7/src/components/Tarefa.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Tarefa() {
+    return (
+        <div>
+            <h2>Relatório de Vendas</h2>
+            <p>Para : Departamento de Vendas</p>
+            <p>Levantar os valores de vendas deste mês.</p>
+        </div>
+    )
+}
