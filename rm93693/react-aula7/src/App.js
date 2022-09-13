731448d61afd4271833c3d7d6985b6e149538d3e
phalves23 <rm93693@fiap.com.br>
2022-09-13 10:01:45 -0300

    :sparkles: desenvolvendo conexão entre as rotas e páginas

--- rm93693/react-aula7/src/App.js
+++ rm93693/react-aula7/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ListaTarefas from "./components/listaTarefas/ListaTarefas";
+import MainRoutes from "./components/routes/MainRoutes";
 
 export default function App() {
   return (
     <>
-      <ListaTarefas />
+      <MainRoutes />
     </>
   );
 }
