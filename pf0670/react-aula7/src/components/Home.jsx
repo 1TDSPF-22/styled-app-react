1bb0074ecc4ee236be263357f75c28b6fc96e4bc
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-13 09:54:42 -0300

    Ajustando rotas

--- pf0670/react-aula7/src/components/Home.jsx
+++ pf0670/react-aula7/src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
             <header>
                 <nav>
-
+                    <Link to="/">Home</Link>
+                    <Link to="/lt">Lista de Tarefas</Link>
+                    <Link to="/add">Adicionar Tarefa</Link>
                 </nav>
             </header>
         </div>
