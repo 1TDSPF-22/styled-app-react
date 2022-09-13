dd402c934b52b6806df7e61103288b1ce4f35a9c
augustopdro <rm94990@fiap.com.br>
2022-09-13 08:47:13 -0300

    Tarefa.jsx add

--- rm94990/react-aula7/src/components/ListaTarefas.jsx
+++ rm94990/react-aula7/src/components/ListaTarefas.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Tarefa from "./Tarefa";
 
 export default function ListaTarefas() {
   return (
     <div>
       <p>Lista de Tarefas</p>
+      <div>
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </div>
     </div>
   );
 }
