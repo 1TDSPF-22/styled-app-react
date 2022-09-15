0d81275ce89308adca4c6bd9c8e8b6cb41277109
igorcamargo <rm94553@fiap.com.br>
2022-09-15 19:31:30 -0300

    Tarefa.jsx add

--- rm94553/aula7/src/components/ListaTarefas.jsx
+++ rm94553/aula7/src/components/ListaTarefas.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Tarefa from "./Tarefa";
 
 export default function ListaTarefas(){
     return(
         <div>
             <p>Lista de tarefas</p>
+            <div>
+                <Tarefa/>
+                <Tarefa/>
+                <Tarefa/>
+            </div>
         </div>
     )
 }
---
+++ rm94553/aula7/src/components/Tarefa.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Tarefa(){
+    return(
+        <div>
+            <h2>Relatório de vendas</h2>
+            <p>Para: departamento de vendas</p>
+            <p>Levantar os valores de vendas deste mês.</p>
+        </div>
+    )
+
+}
