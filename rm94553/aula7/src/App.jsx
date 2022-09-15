86ee847c31f173b18bc1a205002b46573abfd589
igorcamargo <rm94553@fiap.com.br>
2022-09-15 19:26:46 -0300

    index.css + listaTarefa.jsx

--- rm94553/aula7/src/App.jsx
+++ rm94553/aula7/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ListaTarefas from "./components/ListaTarefas";
 
 export default function App(){
   return (
     <div>
       <h1>Estudo styled components</h1>
+      <ListaTarefas/>
     </div>
   )
 
