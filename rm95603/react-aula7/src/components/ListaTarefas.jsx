c72d6e3d2dafc2e758620547b1b0e9c2509fb24c
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-16 11:35:22 -0300

    Adicionando Styled JS

--- rm95603/react-aula7/src/components/ListaTarefas.jsx
+++ rm95603/react-aula7/src/components/ListaTarefas.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from 'react'
 import Tarefa from './Tarefa'
-import styled from 'styled-components'
+import { DivLista } from '../style/styled'
 
-const DivLista = styled.div`
-    width: 100%;
-    min-height: 85vh;
-    background-color: #ffb;
-    padding: 20px;
-    border: 2px solid #ccc;
-    display: flex; flex-wrap: wrap;
-    justify-content: space-around;
-`
+
+
 
 export default function ListaTarefas() {
 
--- rm95603/react-aula7/src/routes/MainRoutes.jsx
+++ rm95603/react-aula7/src/routes/MainRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import FormTarefas from '../components/FormTarefas'
 import Home from '../components/Home'
 import ListaTarefas from '../components/ListaTarefas'
