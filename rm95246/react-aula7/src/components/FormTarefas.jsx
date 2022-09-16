ef689d156386804c7709716a2e83629f6c9a5dbf
btwbeatriz <rm95246@fiap.com.br>
2022-09-16 12:15:55 -0300

    nova tarefa, button, props, useState, att do form

--- rm95246/react-aula7/src/components/FormTarefas.jsx
+++ rm95246/react-aula7/src/components/FormTarefas.jsx
@@ -3,26 +3,29 @@ import React from 'react'
 export default function FormTarefas(props){
     return(
         <div>
-            <form method="post">
+            <form method="post" onSubmit={props.adicionarNovaTarefa}>
+                
                 <fieldset>
                     <legend>Tarefas</legend>
                     <div>
                         <label>Título</label>
-                        <input type="text" name="titulo" placeholder="Título" value="" />
+                        <input type="text" name="titulo" placeholder="Título" value={props.novaTarefa.titulo} onChange={props.digit}/>
                     </div>
                     <div>
                         <label>Setor</label>
-                        <input type="text" name="setor" placeholder="Setor" value="" />
+                        <input type="text" name="setor" placeholder="Setor" value={props.novaTarefa.setor} onChange={props.digit}/>
                     </div>
                     <div>
                         <label>Descrição</label>
-                        <textarea name="descricao" cols="30" rows="10 "placeholder="Descrição" value=""></textarea>
+                        <textarea name="descricao" cols="30" rows="10 "placeholder="Descrição" value={props.novaTarefa.descricao} onChange={props.digit}/>
                     </div>
                     <div>
                         <input type="submit" value="Adicionar" />
                     </div>
                 </fieldset>
+            
             </form>
+        
         </div>
     )
 }
--- rm95246/react-aula7/src/components/ListaTarefas.jsx
+++ rm95246/react-aula7/src/components/ListaTarefas.jsx
@@ -1,17 +1,7 @@
 import React, { useState } from 'react';
 import Tarefa from './Tarefa';
-import styled from 'styled-components';
-
-const DivLista = styled.div`
-    width: 100%;
-    min-height: 85vh;
-    background-color: #ffb;
-    padding: 20px;
-    border: 2px solid #ccc;
-    display: flex; flex-wrap: wrap;
-    justify-content: space-around;
-`
-
+import { DivLista } from '../style/styled'
+import FormTarefas from './FormTarefas';
 
 export default function ListaTarefas(){
 
@@ -33,15 +23,27 @@ export default function ListaTarefas(){
         },
     ])
 
-    //Criando uma função para adicionar tarefas
-    const addTarefa = ()=>{
-        const novaTarefa = {
-            titulo: "Planilha de notas",
-            setor: "Graduação",
-            descricao: "Lançar notas dos exercícios.",
-        }
+    const[nTarefa, setNTarefa] = useState({"titulo" : "", "setor" : "", "descricao" : ""})
 
-        setTarefa([...tarefa, novaTarefa])
+    //Criando uma função para adicionar tarefas
+    const addTarefa = (e)=>{
+        e.preventDefault()
+        setNTarefa({"titulo" : "", "setor" : "", "descricao" : ""})
+        setTarefa([...tarefa, nTarefa])
+    }
+
+    const captura = (e)=>{
+        e.preventDefault()
+        
+        const{name, value} = e.target
+
+        if(name === "titulo"){
+            setNTarefa({"titulo" : value, "setor" : nTarefa.setor, "descricao" : nTarefa.descricao})
+        }else if(name === "setor"){
+            setNTarefa({"titulo" : nTarefa.titulo, "setor" : value, "descricao" : nTarefa.descricao})
+        }else if(name === "descricao"){
+            setNTarefa({"titulo" : nTarefa.titulo, "setor" : nTarefa.setor, "descricao" : value})
+        }
     }
 
     return(
@@ -52,6 +54,13 @@ export default function ListaTarefas(){
                 <button onClick={addTarefa}>ADD-TAREFA</button>
             </div>
             <DivLista>
+                
+                <FormTarefas
+                    digit={captura}
+                    novaTarefa={nTarefa}
+                    adicionarNovaTarefa={addTarefa}
+                />
+                
                 {tarefa.map((t,i)=>
                 <Tarefa
                     key={i}
